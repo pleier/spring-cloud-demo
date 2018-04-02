bb1679b9bfe81d1fe8d1c92e5f90c17085b5f7c1
plei <pleiyang@outlook.com>
2018-04-02 15:51:57 +0800

    Initial commit

--- microservice-consumer-movie-understanding-metadata/src/main/java/com/plerer/controller/MovieController.java
+++ microservice-consumer-movie-understanding-metadata/src/main/java/com/plerer/controller/MovieController.java
@@ -22,7 +22,7 @@ public class MovieController {
 
   @GetMapping("/user/{id}")
   public User findById(@PathVariable Long id) {
-    return this.restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+    return this.restTemplate.getForObject("http://localhost:8001/" + id, User.class);
   }
 
   @GetMapping("/user-instance")
--- microservice-simple-consumer-movie/src/main/java/com/plerer/controller/MovieController.java
+++ microservice-simple-consumer-movie/src/main/java/com/plerer/controller/MovieController.java
@@ -16,5 +16,6 @@ public class MovieController {
   @GetMapping("/user/{id}")
   public User findById(@PathVariable Long id) {
     return this.restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+
   }
 }
