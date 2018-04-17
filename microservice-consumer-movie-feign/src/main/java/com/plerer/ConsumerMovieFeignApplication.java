0c6d82478ce9229f3782a25428c0b071a0336764
plei <pleiyang@outlook.com>
2018-04-17 11:06:29 +0800

    Initial commit

--- microservice-consumer-movie-feign/src/main/java/com/plerer/ConsumerMovieFeignApplication.java
+++ microservice-consumer-movie-feign/src/main/java/com/plerer/ConsumerMovieFeignApplication.java
@@ -2,7 +2,6 @@ package com.plerer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -11,13 +10,8 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
-public class ConsumerMovieApplication {
-  @Bean
-  public RestTemplate restTemplate() {
-    return new RestTemplate();
-  }
-
+public class ConsumerMovieFeignApplication {
   public static void main(String[] args) {
-    SpringApplication.run(ConsumerMovieApplication.class, args);
+    SpringApplication.run(ConsumerMovieFeignApplication.class, args);
   }
 }
